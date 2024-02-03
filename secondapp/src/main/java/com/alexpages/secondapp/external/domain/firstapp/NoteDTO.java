27edbdd9243c5aaac956c4e9e5577894f54741c1
Alex Pages Andreu <99218116+alexpages@users.noreply.github.com>
2024-02-03 18:34:41 +0100

    Update NoteDTO.java

--- secondapp/src/main/java/com/alexpages/secondapp/external/domain/firstapp/NoteDTO.java
+++ secondapp/src/main/java/com/alexpages/secondapp/external/domain/firstapp/NoteDTO.java
@@ -2,8 +2,10 @@ package com.alexpages.secondapp.external.domain.firstapp;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Data
+@AllArgsConstructor	
 @NoArgsConstructor
 public class NoteDTO {
 
